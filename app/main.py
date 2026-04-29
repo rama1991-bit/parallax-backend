fa8a5c8a615fa35185b56520e58a920f0ba6c207
rama1991-bit <ashtary.rasoul@gmail.com>
2026-04-29 15:03:47 +0200

    fix cors for vercel analyze

--- app/main.py
+++ app/main.py
@@ -15,8 +15,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://parallax-frontend.vercel.app",
+        "http://localhost:3000",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
